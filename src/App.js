27176ee5cb01453531dc7a31e1b4312ba0cdbdfb
seif <seenaseif@gmail.com>
2024-03-28 00:58:02 +0200

    last

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (Cookies.get("id") === undefined) {
+      navigate("/addid");
+    }
+  }, []);
   return (
     <div className="App">
       <Navbar />
